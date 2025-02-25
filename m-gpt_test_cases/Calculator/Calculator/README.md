fb1d4d4d7de363f711351870c2ed433301f89be2
sravyasambaturu <sravya.sambaturu75@gmail.com>
2025-02-24 19:51:18 -0500

    adding calc test cases from mgpt 4o

---
+++ m-gpt_test_cases/Calculator/Calculator/README.md
@@ -0,0 +1,15 @@
+mvn clean clover:setup test clover:aggregate clover:clover
+
+mvn test-compile org.pitest:pitest-maven:mutationCoverage
+
+have the context before (ATM test cases)
+
+This is my java code for calculator class can you write unit test cases with coverage greater than 86% 
+
+![img.png](img.png)
+
+![img_1.png](img_1.png)
+
+![img_2.png](img_2.png)
+
+![img_3.png](img_3.png)
--- m-gpt_test_cases/Calculator/Calculator/src/test/java/org/example/CalculatorTest.java
+++ m-gpt_test_cases/Calculator/Calculator/src/test/java/org/example/CalculatorTest.java
@@ -1,5 +1,4 @@
-package org.example;
-
+import org.example.Calculator;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,7 +6,7 @@ import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
-    Calculator calculator;
+    private Calculator calculator;
 
     @Before
     public void setUp() {
@@ -15,8 +14,82 @@ public class CalculatorTest {
     }
 
     @Test
-    public void divide() {
-        double result = calculator.divide(10, 2);
-        assertEquals(5, result, 0);
+    public void testSum() {
+        double[] values = {1.0, 2.0, 3.0};
+        assertEquals(6.0, calculator.sum(values), 0.0001);
+    }
+
+    @Test
+    public void testSubtract() {
+        double[] values = {5.0, 2.0, 1.0};
+        assertEquals(2.0, calculator.subtract(values), 0.0001);
+    }
+
+    @Test
+    public void testMultiplication() {
+        double[] values = {2.0, 3.0, 4.0};
+        assertEquals(24.0, calculator.Multiplication(values), 0.0001);
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2.5, calculator.divide(5.0, 2.0), 0.0001);
+    }
+
+    @Test
+    public void testDivideByZero() {
+        assertEquals(Double.MIN_VALUE, calculator.divide(5.0, 0.0), 0.0001);
+    }
+
+    @Test
+    public void testSquareRoot() {
+        assertEquals(3.0, calculator.squareRoot(9.0), 0.0001);
+    }
+
+    @Test
+    public void testSquareRootOfNegative() {
+        assertEquals(Double.MIN_VALUE, calculator.squareRoot(-9.0), 0.0001);
+    }
+
+    @Test
+    public void testModuloOfTwoNum() {
+        assertEquals(1.0, calculator.moduloOfTwoNum(7.0, 3.0), 0.0001);
+    }
+
+    @Test
+    public void testModuloOfTwoNumWithNegative() {
+        assertEquals(1.0, calculator.moduloOfTwoNum(-7.0, 3.0), 0.0001);
+    }
+
+    @Test
+    public void testModuloOfTwoNumByZero() {
+        assertEquals(Double.MIN_VALUE, calculator.moduloOfTwoNum(7.0, 0.0), 0.0001);
+    }
+
+    @Test
+    public void testAverage() {
+        double[] values = {2.0, 4.0, 6.0};
+        assertEquals(4.0, calculator.Average(values), 0.0001);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAverageEmptyArray() {
+        double[] values = {};
+        calculator.Average(values);
+    }
+
+    @Test
+    public void testFactorialPositive() {
+        assertEquals(120, calculator.factorial(5));
+    }
+
+    @Test
+    public void testFactorialZero() {
+        assertEquals(1, calculator.factorial(0));
+    }
+
+    @Test
+    public void testFactorialNegative() {
+        assertEquals(0, calculator.factorial(-5));
     }
 }
